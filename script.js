2dade762d981ef1925219c89bdbee71a587c3d2b
rulo-dev <rulosan77@gmail.com>
2024-12-26 22:53:35 -0600

    agregando script con ejercicios de operadores

---
+++ script.js
@@ -0,0 +1,40 @@
+let a = 5;
+let b = 10;
+
+if (a > b) {
+  console.log('a is greater than b');
+}
+
+if (a < b) {
+  console.log('a is less than b');
+}
+
+if (a === b) {
+  console.log('estas mal mi chavo, revisa tu cerebro');
+} else {
+  console.log('estas bien mi chavo, sigue asi');
+}
+
+/*declaracion de variables */
+let nombre = 'Pedro',
+  puntuacion = 0,
+  record;
+record = 5000;
+
+console.log(nombre, puntuacion, record);
+
+/*operadores*/
+console.log(10 + 10);
+console.log(20 - 10);
+console.log(10 * 10);
+console.log(20 / 10);
+console.log(5 + 6 * 3);
+console.log((5 + 6) * 3);
+
+let precioUnidad, cantidad, total;
+
+precioUnidad = 3.5;
+cantidad = 5;
+total = precioUnidad * cantidad;
+
+console.log('El total de la compra es: ' + total);
